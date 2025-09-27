9f6bdb4114de0bd8163c3cea8bfab1bfa0c58f53
lydiacasa <https://github.com/Lydiafg2002/3d_2526_E01>
2025-09-27 21:31:28 +0200

    enemigo muere

--- Assets/scrips/Bullet.cs
+++ Assets/scrips/Bullet.cs
@@ -4,10 +4,11 @@ public class Bullet : MonoBehaviour
 {
     public float BulletSpeed;
     public Rigidbody rb;
-
+    public GameObject Player;
 
     private void Start()
     {
+        Player = GameObject.Find("Player");
         Destroy(gameObject, 5);
     }
     private void Update()
@@ -16,6 +17,12 @@ public class Bullet : MonoBehaviour
     }
     private void OnTriggerEnter(Collider other)
     {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            Player.GetComponent<Score>().MorePoints(1);
+            Destroy(other.gameObject);
+        }
        
     }
 }
+
